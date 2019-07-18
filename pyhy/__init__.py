7a5172f8d43e91aad8cb1acd904f2a2261953811
Ryan Breaker <ryan@breaker.rocks>
2019-07-18 09:05:16 -0500

    Implement hydro_sign_verify
    Implements hydro_sign_verify for single-part signature verification.

--- pyhy/__init__.py
+++ pyhy/__init__.py
@@ -38,6 +38,7 @@ __all__ = [
     # sign
     'hydro_sign',
     'hydro_sign_create',
+    'hydro_sign_verify',
     'hydro_sign_keygen',
     'hydro_sign_keygen_deterministic',
     # kx
@@ -292,6 +293,12 @@ def hydro_sign_create(m, ctx, secret_key):
     mlen = len(m)
     h.hydro_sign_create(buf, m.encode('utf8'), mlen, ctx.encode('utf8'), secret_key)
     return bytes(buf)
+
+def hydro_sign_verify(sig, m, ctx, public_key):
+    assert (type(ctx) == str) and (len(ctx) == h.hydro_kdf_CONTEXTBYTES)
+    mlen = len(m)
+    result = h.hydro_sign_verify(sig, m.encode('utf-8'), mlen, ctx.encode('utf-8'), public_key)
+    return result == 0
 
 class hydro_sign(object):
     """wrapper class for signature creation, verification"""
