3077eaee5b3eb130fb9b02b9a11de390349df70c
Jeff H <jeffrey92@gmail.com>
2019-10-16 15:56:20 -0700

    fix pip packaging mess

--- setup.py
+++ setup.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
-from setuptools import find_packages, setup
+import os
+from codecs import open
+from setuptools import setup
 
 NAME = 'pyhy'
-VERS = '0.0.7'
 PACKAGES = [ 'pyhy' ]
 SHORT_DESC = 'bindings for libhydrogen using cffi'
-LONG_DESC = """Python bindings for libhydrogen, a small cryptographic library
-suited for embedded systems requiring a low footprint. Uses cffi.
-"""
 KEYWORDS = [ 'cryptography', 'crypto', 'embedded', 'encryption', 'libhydrogen', 'hydrogen' ]
 # http://pypi.python.org/pypi?%3Aaction=list_classifiers
 CLASSIFIERS = [
@@ -23,14 +21,22 @@ CLASSIFIERS = [
     'Topic :: Software Development :: Embedded Systems'
 ]
 
+about = {}
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'pyhy', '__version__.py'), 'r', 'utf-8') as f:
+    exec(f.read(), about)
+
+with open('README.md', 'r', 'utf-8') as f:
+    readme = f.read()
+
 # https://packaging.python.org/tutorials/distributing-packages/#setup-py
 # https://github.com/pypa/sampleproject/blob/master/setup.py
 if __name__ == '__main__':
     setup(
         name=NAME,
-        version=VERS,
+        version=about['__version__'],
         description=SHORT_DESC,
-        long_description=LONG_DESC,
+        long_description=readme,
         license='BSD',
         url='https://github.com/someburner/pyhy',
         author='Jeff Hufford',
@@ -42,10 +48,8 @@ if __name__ == '__main__':
         setup_requires=["cffi>=1.0.0"],
         cffi_modules=["./bind.py:ffibuilder"],
         install_requires=["cffi>=1.0.0"],
-        include_package_data=True,
-        packages=['', 'pyhy'],
+        packages=PACKAGES,
         package_data={
-            '': [ '*.h' ],
             'pyhy': [ './*.py' ]
         }
     )
